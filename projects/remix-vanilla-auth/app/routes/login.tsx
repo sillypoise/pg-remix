b8440c0791133ff3a7346474c13cfb8b2f53cdc7
sillypoise <somewhatjose@gmail.com>
2022-11-01 11:11:13 -0500

    p/remix-vanilla-auth: Let's pause this for a moment

--- projects/remix-vanilla-auth/app/routes/login.tsx
+++ projects/remix-vanilla-auth/app/routes/login.tsx
@@ -1,5 +1,5 @@
-import { redirect, type ActionArgs } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { json, LoaderArgs, redirect, type ActionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { commitSession, getSession } from "~/session";
 
 export async function action({ request }: ActionArgs) {
@@ -22,7 +22,7 @@ export async function action({ request }: ActionArgs) {
     let session = await getSession(request.headers.get("Cookie"));
 
     if (errors.length) {
-        session.set("errors", JSON.stringify(errors));
+        session.flash("errors", JSON.stringify(errors));
         return redirect("/login", {
             headers: {
                 "Set-Cookie": await commitSession(session),
@@ -32,7 +32,16 @@ export async function action({ request }: ActionArgs) {
     return redirect("/");
 }
 
+export async function loader({ request }: LoaderArgs) {
+    let session = await getSession(request.headers.get("Cookie"));
+
+    return json({ session: JSON.parse(session.data.errors) });
+}
+
 export default function Login() {
+    let { session } = useLoaderData();
+    console.log(session);
+
     return (
         <main className="mlb-l">
             <article className="center stack">
@@ -47,6 +56,7 @@ export default function Login() {
                         placeholder="name@mail.com"
                         className="max-is-[20ch] rounded-md p-3xs"
                     />
+                    <pre>{JSON.stringify(session[0])}</pre>
                     <label htmlFor="signup:pw">Password</label>
                     <input
                         type="password"
@@ -55,6 +65,7 @@ export default function Login() {
                         // required
                         className="max-is-[20ch] rounded-md p-3xs"
                     />
+                    <pre>{JSON.stringify(session[1])}</pre>
                     <button
                         type="submit"
                         className="max-is-[20ch] rounded-md bg-dark-gray-5 hover:bg-dark-gray-6"
