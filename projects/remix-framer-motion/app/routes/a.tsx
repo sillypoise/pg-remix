c724dca9ae7550633fee1b16129ddded9bbd8e2a
sillypoise <somewhatjose@gmail.com>
2022-11-09 12:19:57 -0500

    p/remix-framer-motion: setting up route transition example

---
+++ projects/remix-framer-motion/app/routes/a.tsx
@@ -0,0 +1,9 @@
+export default function PageA() {
+    return (
+        <main className="mlb-l bg-dark-pink-8">
+            <article className="center stack max-is-[50ch] cover">
+                <h2 className="text-3">A</h2>
+            </article>
+        </main>
+    );
+}
---
+++ projects/remix-framer-motion/app/routes/b.tsx
@@ -0,0 +1,9 @@
+export default function PageB() {
+    return (
+        <main className="mlb-l bg-dark-blue-9">
+            <article className="center stack max-is-[50ch] cover">
+                <h2 className="text-3">B</h2>
+            </article>
+        </main>
+    );
+}
