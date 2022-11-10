b440aefa96e175719a7b5f16fb95b53202ed6e76
sillypoise <somewhatjose@gmail.com>
2022-11-10 15:50:49 -0500

    p/remix-framer-motion: cleaned up a bit the dialog animation

--- projects/remix-framer-motion/app/routes/menu/index.tsx
+++ projects/remix-framer-motion/app/routes/menu/index.tsx
@@ -9,55 +9,48 @@ import { useState } from "react";
 export default function MenuIndex() {
     let [isOpen, setIsOpen] = useState(false);
     return (
-        <article className="center stack">
-            <h3>hey</h3>
-            <p>{isOpen ? "open" : "not open"}</p>
-            <Dialog.Root open={isOpen}>
-                <Dialog.Trigger
-                    className="bg-[color:var(--background)] | debug"
-                    onClick={() => setIsOpen(!isOpen)}
-                >
-                    OpenMenu
-                </Dialog.Trigger>
-                <AnimatePresence mode="wait" initial={false}>
-                    {isOpen ? (
-                        <Dialog.Portal forceMount>
-                            <Dialog.Content asChild>
-                                <motion.div
-                                    initial={{ opacity: 0, x: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    exit={{ opacity: 0 }}
-                                    transition={{ duration: 1 }}
-                                    className="fixed bg-dark-sky-8 inset-[0] z-10"
-                                >
-                                    <div className="center stack items-center">
-                                        <nav className="mbs-2xl nav">
-                                            <ul
-                                                role="list"
-                                                className="stack [--stack-gap:theme(spacing.m)] "
-                                            >
-                                                <li>
-                                                    <Link to="/">Home</Link>
-                                                </li>
-                                                <li>
-                                                    <Dialog.Close
-                                                        className="bg-[color:var(--background)]"
-                                                        onClick={() =>
-                                                            setIsOpen(!open)
-                                                        }
-                                                    >
-                                                        X
-                                                    </Dialog.Close>
-                                                </li>
-                                            </ul>
-                                        </nav>
-                                    </div>
-                                </motion.div>
-                            </Dialog.Content>
-                        </Dialog.Portal>
-                    ) : null}
-                </AnimatePresence>
-            </Dialog.Root>
-        </article>
+        <Dialog.Root open={isOpen}>
+            <Dialog.Trigger
+                className="bg-[color:var(--background)] | debug"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                OpenMenu
+            </Dialog.Trigger>
+            <AnimatePresence mode="wait" initial={false}>
+                {isOpen ? (
+                    <Dialog.Portal forceMount>
+                        <Dialog.Content asChild>
+                            <motion.div
+                                initial={{ opacity: 0, x: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 1 }}
+                                className="fixed bg-dark-sky-8 inset-[0] z-10"
+                            >
+                                <div className="center stack items-center">
+                                    <nav className="mbs-2xl nav">
+                                        <ul role="list" className="stack ">
+                                            <li>
+                                                <Link to="/">Home</Link>
+                                            </li>
+                                            <li>
+                                                <Dialog.Close
+                                                    className="bg-transparent"
+                                                    onClick={() =>
+                                                        setIsOpen(!open)
+                                                    }
+                                                >
+                                                    X
+                                                </Dialog.Close>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </motion.div>
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                ) : null}
+            </AnimatePresence>
+        </Dialog.Root>
     );
 }
