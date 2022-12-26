b309ed5b00d35d91399dd4609180a08be658fe61
sillypoise <somewhatjose@gmail.com>
2022-12-26 17:20:25 -0500

    remix-s3: a lot of trial and error and a lot of pain lol

---
+++ projects/remix-s3/app/routes/upload.tsx
@@ -0,0 +1,52 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+    type ActionArgs,
+    unstable_createMemoryUploadHandler as createMemoryUploadHandler,
+    type UploadHandler,
+    FormData,
+    writeAsyncIterableToWritable,
+} from "@remix-run/node";
+import {
+    json,
+    unstable_parseMultipartFormData as parseMultipartFormData,
+    unstable_composeUploadHandlers as composeUploadHandlers,
+} from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
+import { PassThrough } from "stream";
+import { s3, s3UploadHandler, uploadStreamToS3 } from "~/utils/s3.server";
+
+export async function action({ request, context }: ActionArgs) {
+    function uploadHandler() {
+        return "zumba";
+    }
+    const formData = await parseMultipartFormData(request, uploadHandler);
+
+    console.log(formData.get("img"));
+    return {};
+}
+
+export default function Upload() {
+    const fetcher = useFetcher();
+    return (
+        <main className="mbs-l">
+            <article className="center stack">
+                <h2 className="text-2">File Upload</h2>
+                <fetcher.Form method="post" encType="multipart/form-data">
+                    <fieldset className="stack">
+                        <label htmlFor="img-field">Image to upload</label>
+                        <input
+                            id="img-field"
+                            type="file"
+                            name="img"
+                            accept="image/*"
+                            // multiple
+                        />
+                        <label htmlFor="img-desc">Image description</label>
+                        <input id="img-desc" type="text" name="desc" />
+                        <button type="submit">Upload to S3</button>
+                    </fieldset>
+                </fetcher.Form>
+            </article>
+        </main>
+    );
+}
---
+++ projects/remix-s3/app/utils/s3.server.ts
@@ -0,0 +1,61 @@
+import {
+    type PutObjectCommandInput,
+    PutObjectCommand,
+    S3Client,
+} from "@aws-sdk/client-s3";
+import AWS from "aws-sdk";
+import {
+    type UploadHandler,
+    writeAsyncIterableToWritable,
+} from "@remix-run/node";
+import { PassThrough } from "stream";
+
+const R2_ENDPOINT = process.env["R2_ENDPOINT"];
+const R2_ACCESS_KEY = process.env["R2_ACCESS_KEY"];
+const R2_SECRET_KEY = process.env["R2_SECRET_KEY"];
+
+if (!(R2_ENDPOINT && R2_ACCESS_KEY && R2_SECRET_KEY)) {
+    throw new Error("Storage is missing required configuration");
+}
+
+const s3v3 = new S3Client({
+    region: "auto",
+    endpoint: R2_ENDPOINT,
+    credentials: {
+        accessKeyId: R2_ACCESS_KEY,
+        secretAccessKey: R2_SECRET_KEY,
+    },
+});
+
+async function uploadStreamToS3(
+    data: any,
+    filename: string,
+    contentType: string
+) {
+    const stream = uploadStream({
+        Key: filename,
+        ContentType: contentType,
+    });
+    await writeAsyncIterableToWritable(data, stream.writeStream);
+    const file = await stream.promise;
+    return file.Location;
+}
+
+const s3UploadHandler: UploadHandler = async ({
+    name,
+    filename,
+    data,
+    contentType,
+}) => {
+    if (name !== "img") {
+        return undefined;
+    }
+    const uploadedFileLocation = await uploadStreamToS3(
+        data,
+        filename!,
+        contentType
+    );
+    return uploadedFileLocation;
+};
+
+export { s3v3, uploadStreamToS3, s3UploadHandler };
