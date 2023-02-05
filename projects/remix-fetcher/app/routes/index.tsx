4b1cb4a09d8eec72faf0af4247fa01b244724042
sillypoise <somewhatjose@gmail.com>
2023-02-05 10:29:23 -0500

    main: remix-fetcher init

---
+++ projects/remix-fetcher/app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return (
+        <article className="center mlb-xl stack">
+            <h1 className="text-3">Remix Fetcher Patterns</h1>
+            <p>Learning client-side fetching patterns inside remix</p>
+            <h2 className="text-2">Let's use</h2>
+        </article>
+    );
+}
